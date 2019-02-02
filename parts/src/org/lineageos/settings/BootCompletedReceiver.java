22b4aaeb813fe17ebfc634b549d02f33dceaff51
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-02 14:31:51 -0200

    lavender: XiaomiParts: Only call setEnabled on boot completed

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -35,6 +35,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             if (DEBUG) Log.d(TAG, "Starting Doze service");
             DozeUtils.startService(context);
         }
-        new DiracUtils(context);
+        new DiracUtils(context).onBootCompleted();
     }
 }
