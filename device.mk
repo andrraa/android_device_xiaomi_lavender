0d6cd2aabd4c5ab489e703ca592b745771568cd6
rajadeja <raj.jdja@gmail.com>
2019-10-26 07:33:35 +0000

    lavender: Add authsecret interface
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- device.mk
+++ device.mk
@@ -143,6 +143,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# AuthSecret
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
