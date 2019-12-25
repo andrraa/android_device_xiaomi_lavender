73c5c4f2b1367353ca5c5073bd4f3c6740cb2349
Dyneteve <dyneteve@pixelexperience.org>
2019-12-25 16:33:01 +0200

    lavender: Add NexusLauncherRelease to PRODUCT_DEXPREOPT_SPEED_APPS

--- device.mk
+++ device.mk
@@ -379,9 +379,10 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libprotobuf-cpp-full
 
-# Preopt SystemUI
+# AOT Preload
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI
+    SystemUI \
+    NexusLauncherRelease
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
