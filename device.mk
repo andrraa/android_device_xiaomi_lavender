c1f9ce144b3d1955864d3c93b53c9c88552e2a31
rena2806 <renascape@gmail.com>
2020-04-18 10:56:22 +0200

    lavender: Build GoogleCameraGO

--- device.mk
+++ device.mk
@@ -147,7 +147,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.common@1.0 \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    CameraGo
 
 # Camera  Shims
 PRODUCT_PACKAGES += \
