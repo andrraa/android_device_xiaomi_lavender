7cd08d2a089c848582b6289a7da4f53e0409dba8
andrraa <andraserver01@gmail.com>
2020-05-07 04:04:10 +0700

    lavender: Revert "lavender: Update GPS from lavender-user-10-QKQ1.190910.002-20.3.11-release-keys"
    This reverts commit 789ddfd91430b4595663bac6e5a0db4019503f7d.

--- device.mk
+++ device.mk
@@ -235,9 +235,10 @@ PRODUCT_ODM_PROPERTIES += \
 # Vendor SPL
 VENDOR_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
 
-# GPS Config
+# GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
@@ -245,8 +246,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 PRODUCT_PACKAGES += \
-    libcurl \
-    libsensorndkbridge
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti \
+    libgnss \
+    libgnsspps \
+    libsensorndkbridge \
+    libvehiclenetwork-native
 
 # Google Assistant + ANGLE
 PRODUCT_PRODUCT_PROPERTIES += \
