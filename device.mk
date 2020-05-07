6e895cb4371e7d3483f9924b40febc48bd619e64
faham1997 <nafidfaham08@gmail.com>
2020-05-07 06:12:01 +0600

    lavender: Drop Safailnet

--- device.mk
+++ device.mk
@@ -429,8 +429,7 @@ PRODUCT_PACKAGES += \
     init.qcom.sensors.sh \
     init.target.rc \
     ueventd.qcom.rc \
-    fstab.qcom \
-    init.safailnet.rc
+    fstab.qcom
 
 # RCS
 PRODUCT_PACKAGES += \
