077ce584ecf92eb2d31f4493f7d1cc0059f116e5
Genkzsz11 <Genkzsz11@gmail.com>
2020-05-15 02:40:39 +0700

    lavender: Uprev radio config to 1.2

--- device.mk
+++ device.mk
@@ -452,7 +452,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
     android.hardware.radio@1.2 \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.2 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
