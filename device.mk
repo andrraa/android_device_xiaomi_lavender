62167b838c2014d130a7c8b070f734e201776719
andrraa <andraserver01@gmail.com>
2020-06-01 18:55:37 +0700

    lavender: Drop GoogleCameraGO * Better use GCam instead of GcamGO

--- device.mk
+++ device.mk
@@ -154,8 +154,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.common@1.0 \
-    vendor.qti.hardware.camera.device@1.0 \
-    CameraGo
+    vendor.qti.hardware.camera.device@1.0
 
 # Camera  Shims
 PRODUCT_PACKAGES += \
