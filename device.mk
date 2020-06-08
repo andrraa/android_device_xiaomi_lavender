08e7f0d26aec82ca1269945975ae2100d00501c6
andrraa <andraserver01@gmail.com>
2020-06-08 19:12:06 +0700

    lavender: Revert "lavender: Build new GNSS HAL"
    This reverts commit d6926c3b98f0564e39e184449ec728f4e23c6a75.

--- device.mk
+++ device.mk
@@ -234,28 +234,22 @@ PRODUCT_ODM_PROPERTIES += \
 VENDOR_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
 
 # GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+
 PRODUCT_PACKAGES += \
-    libbatching \
-    libcurl \
-    libgeofencing \
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti \
     libgnss \
-    libgps.utils \
-    liblocation_api \
+    libgnsspps \
     libsensorndkbridge \
     libvehiclenetwork-native
-
-PRODUCT_PACKAGES += \
-    apdr.conf \
-    flp.conf \
-    gps.conf \
-    izat.conf \
-    lowi.conf \
-    sap.conf \
-    xtwifi.conf
-
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.0-impl-qti \
-    android.hardware.gnss@2.0-service-qti
 
 # Google Assistant + ANGLE
 PRODUCT_PRODUCT_PROPERTIES += \
